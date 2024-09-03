46b78423f143aeb15749ede634cb239550446094
Synchrozine <baturay.gok@gmail.com>
2024-09-03 01:19:46 -0500

    QoL

--- Week2/bowling.cpp
+++ Week2/bowling.cpp
@@ -1,3 +1,16 @@
+/*
+You want to calculate how tall a building is and have determined that the best way is to roll a bowling ball off the
+roof of the building and time how many seconds it takes to hit the ground.  The height of the building in meters can be
+calculated as follows:
+
+h = 1/2 g t2
+h is the height of the building in meters
+g is acceleration due to gravity which is equal to 9.8
+t is the number of seconds it takes the bowling ball to hit the ground
+Write a C++ program that will prompt the user for the number of seconds and will calculate the height of the building.
+*/
+
+
 # include <iostream>
 # include <cmath>
 
