f68f9c15261559cc33824ff9420f6e4748215e7f
Synchrozine <baturay.gok@gmail.com>
2024-09-17 20:27:51 -0500

    Functions

--- week4/shapes.cpp
+++ week4/shapes.cpp
@@ -12,31 +12,21 @@ const int QUIT = 6;
 
 //Prototyping
 double ReadDimension(string prompt);
-
 double CubeArea(double length);
-
 double CubeVolume(double length);
-
 double SphereArea(double radius);
-
 double SphereVolume(double radius);
-
 double PrismArea(double length, double width, double height);
-
 double PrismVolume(double length, double width, double height);
-
 double CylinderArea(double radius, double height);
-
 double CylinderVolume(double radius, double height);
-
 double ConeArea(double radius, double height);
-
 double ConeVolume(double radius, double height);
 
 int main() {
     int menu = 0;
 
-    cout << "1. Cube\n2.Sphere\n3.Prism\n4.Cylinder\n5.Cone\n6.Quit\nEnter choice: ";
+    cout << "1. Cube\n2.Sphere\n3.Prism(Right Rectangular)\n4.Cylinder\n5.Cone\n6.Quit\nEnter choice: ";
     cin >> menu;
 
     while (menu != QUIT) {
@@ -45,14 +35,22 @@ int main() {
             cout << "Volume is : " << CubeVolume(length) << endl;
             cout << "Area is : " << CubeArea(length) << endl;
         }
+
         if (menu == SPHERE) {
+            double radius = ReadDimension("Enter sphere radius: ");
+            cout << "Volume is : " << SphereVolume(radius) << endl;
+            cout << "Area is : " << SphereArea(radius) << endl;
         }
+
         if (menu == PRISM) {
         }
+
         if (menu == CYLINDER) {
         }
+
         if (menu == CONE) {
         }
+
 
         if (menu != CUBE && menu != SPHERE && menu != PRISM && menu != CYLINDER && menu != CONE) {
             cout << "Invalid menu choice\n";
@@ -83,9 +81,41 @@ double ReadDimension(string prompt) {
 
 
 double CubeArea(double length) {
-    return 6.0 * pow(length, 2);
+    return 6 * pow(length, 2);
 }
 
 double CubeVolume(double length) {
     return pow(length, 3);
+}
+
+double SphereArea(double radius) {
+    return 4 * M_PI * pow(radius, 2);
+}
+
+double SphereVolume(double radius) {
+    return 4 * (M_PI * pow(radius, 3)) /3;
+}
+
+double PrismArea(double length, double width, double height) {
+    return 2 * (length * width + height * length + width * height);
+}
+
+double PrismVolume(double length, double width, double height) {
+    return length * width * height;
+}
+
+double CylinderArea(double radius, double height) {
+    return 2 * (M_PI * radius * height) + 2 * (M_PI * pow(radius, 2));
+}
+
+double CylinderVolume(double radius, double height) {
+    return M_PI * pow(radius,2) * height;
+}
+
+double ConeArea(double radius, double height) {
+    return M_PI * radius * (radius + sqrt(pow(height,2)+pow(radius,2)));
+}
+
+double ConeVolume(double radius, double height) {
+    return M_PI * pow(radius, 2) * height / 3;
 }
